cd73171b83c73b58f68810f7827232ee2d4345c6
amidvidy <amidvidy@gmail.com>
2013-02-02 19:22:24 -0800

    do something with the canvas

--- whiteboard.js
+++ whiteboard.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-    console.log('foo');
+    var board = document.getElementById("board");
+    var ctx = board.getContext("2d");
+    ctx.fillRect(50, 25, 150, 100);
+
 });
