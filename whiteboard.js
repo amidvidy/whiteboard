14d70b5b5a647711e3f813b2d416b8409b4d1767
amidvidy <amidvidy@gmail.com>
2013-02-02 21:44:56 -0800

    use relative coordinates to make things less janky

--- whiteboard.js
+++ whiteboard.js
@@ -2,12 +2,14 @@ $(document).ready(function() {
     var board = document.getElementById("board");
     var ctx = board.getContext("2d");
     ctx.beginPath();
-    $("#board").click(function(event) {
-        console.log('foo');
 
-        var mouseX = event.pageX;
-        var mouseY = event.pageY;
+    $("#board").click(function(event) {
+        var offset = $(this).offset();
+        var mouseX = event.pageX - offset.left;
+        var mouseY = event.pageY - offset.top;
+
         ctx.lineTo(mouseX, mouseY);
+        ctx.moveTo(mouseX, mouseY);
         ctx.closePath();
         ctx.stroke();
     });
