1e2c50434478baca2ea6b80cabcb59692812210f
amidvidy <amidvidy@gmail.com>
2013-02-02 21:40:32 -0800

    very buggy line drawing

--- whiteboard.js
+++ whiteboard.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
     var board = document.getElementById("board");
     var ctx = board.getContext("2d");
-    ctx.fillRect(50, 25, 150, 100);
+    ctx.beginPath();
+    $("#board").click(function(event) {
+        console.log('foo');
 
+        var mouseX = event.pageX;
+        var mouseY = event.pageY;
+        ctx.lineTo(mouseX, mouseY);
+        ctx.closePath();
+        ctx.stroke();
+    });
 });
