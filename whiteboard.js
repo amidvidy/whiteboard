1cbab6a59ea506fa1c62bb94b13444f25617f362
Adam Midvidy <amidvidy@gmail.com>
2013-02-18 17:34:41 -0800

    finished refactor, but need to work on efficiency. No reason to redraw the board each time.

--- whiteboard.js
+++ whiteboard.js
@@ -1,9 +1,17 @@
-// This code isn't going to be pretty. Just trying to get a feel for the html5 canvas API.
+// This code isn't going to be pretty.
+//Just trying to get a feel for the html5 canvas API.
+
+// Some of the code is from here:
+// http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/
 $(document).ready(function() {
 
     // Whiteboard constructor
     var Whiteboard = function(canvas) {
         this.canvas = canvas;
+        this.$canvas = $(canvas);
+        this.offsetLeft = this.$canvas.offset().left;
+        this.offsetTop = this.$canvas.offset().top;
+
         this.ctx = canvas.getContext("2d");
         this.history = [];
 
@@ -11,6 +19,39 @@ $(document).ready(function() {
 
         this.lineColor = '#000000'
         this.lineWidth = 3.0;
+
+        this.isDrawing = false;
+
+        // Helper function to calculate mouse position
+        var mousePos = function(event) {
+            return {
+                x: event.pageX - this.offsetLeft,
+                y: event.pageY - this.offsetTop
+            };
+        }.bind(this);
+
+        // bind event handlers
+        this.$canvas.mousedown(function(event) {
+            this.isDrawing = true;
+            this.stateChange(mousePos(event));
+            this.redraw();
+        }.bind(this));
+
+        this.$canvas.mousemove(function(event) {
+            if (this.isDrawing) {
+                this.stateChange(mousePos(event));
+            }
+            this.redraw();
+        }.bind(this));
+
+        this.$canvas.mouseup(function(event) {
+            this.isDrawing = false;
+        }.bind(this));
+
+        this.$canvas.mouseleave(function(event) {
+            this.isDrawing = false;
+        }.bind(this));
+
     };
 
     Whiteboard.prototype.resetBoard = function() {
@@ -19,28 +60,41 @@ $(document).ready(function() {
 
     Whiteboard.prototype.redraw = function() {
         // Draw all the segments!
+        var curEvent, prevEvent;
         this.resetBoard();
         for (var i = 1; i < this.history.length; i++) {
-            this.drawSegment(this.history[i-1], this.history[i])
+            curEvent = this.history[i];
+            prevEvent = this.history[i-1];
+
+            if (curEvent.isDrawing) {
+                // This event was due to a mouse drag when mouse was already down
+                this.drawSegment(prevEvent.pos, curEvent.pos,
+                                 curEvent.color, curEvent.width);
+            } else {
+                // This event was due to a mouse click
+                this.drawSegment({x: curEvent.pos.x-1, y: curEvent.pos.y},
+                                 curEvent.pos, curEvent.color, curEvent.width);
+            }
         }
     };
 
-    Whiteboard.prototype.drawSegment = function(start, end) {
+    Whiteboard.prototype.drawSegment = function(start, end, color, width) {
         this.ctx.beginPath();
-        this.ctx.lineWidth = start.lineWidth;
-        this.ctx.strokeStyle = start.lineColor;
+        this.ctx.lineWidth = width;
+        this.ctx.strokeStyle = color;
         this.ctx.moveTo(start.x, start.y);
         this.ctx.lineTo(end.x, end.y)
         this.ctx.closePath();
         this.ctx.stroke();
     };
 
-    Whiteboard.prototype.addSegment = function(event) {
-        event.lineColor = this.lineColor;
-        event.lineWidth = this.lineWidth;
-
-        this.history.push(event)
-        this.redraw();
+    Whiteboard.prototype.stateChange = function(pos) {
+        this.history.push({
+            pos: pos,
+            color: this.lineColor,
+            width: this.lineWidth,
+            drawing: this.isDrawing
+        });
     };
 
     Whiteboard.prototype.setLineWidth = function(width) {
@@ -65,14 +119,6 @@ $(document).ready(function() {
 
     // Initialize state
     var mouseDown = false;
-
-    var mousePos = function(event) {
-        var offset = $(this).offset();
-        return {
-            x: event.pageX - offset.left,
-            y:  event.pageY - offset.top
-        };
-    };
 
     // Style for the lazy
     $board.css('border', '1px solid black');
@@ -99,27 +145,6 @@ $(document).ready(function() {
     // reset button
     $resetButton.click(function() {
         wb.resetBoard();
-    });
-
-    // drawing events
-    $board.mousedown(function(event) {
-        // Start drawing
-        mouseDown = true;
-    });
-
-    $board.mouseup(function() {
-        mouseDown = false;
-        curSegment = {};
-    });
-
-    $board.mousemove(function(event) {
-        var pos;
-
-        if (mouseDown) {
-            // Record segment
-            pos = mousePos.call(this, event);
-            wb.addSegment(pos);
-        }
     });
 
 });
